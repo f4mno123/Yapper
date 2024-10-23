f2272336abd69260bb89e1924eead1143155587e
f4mno123 <f4mno123@gmail.com>
2024-10-23 19:29:22 +0200

    project ini

---
+++ src/main/java/com/web/yapper/YapperApplication.java
@@ -0,0 +1,13 @@
+package com.web.yapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YapperApplication.class, args);
+	}
+
+}
