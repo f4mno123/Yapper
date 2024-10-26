51b46d6a63fdb79e46c64a3321900263a8484e9f
f4mno123 <f4mno123@gmail.com>
2024-10-26 16:53:22 +0200

    working login and register

---
+++ src/main/java/com/web/yapper/dtos/RegisterUserDto.java
@@ -0,0 +1,15 @@
+package com.web.yapper.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegisterUserDto {
+    private String email;
+
+    private String password;
+
+    private String fullName;
+
+}
